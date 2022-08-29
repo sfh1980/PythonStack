3c5e68f2ce9c8f39d05b8629c9cc53cc51d61312
Sean <sfh1980@yahoo.com>
2022-08-29 15:08:46 -0400

    first commit

---
+++ test/test.py
@@ -0,0 +1,22 @@
+#empty_list = []
+#ninjas = ['Rozen', 'KB', 'Oliver']
+#print(ninjas[2])
+#ninjas[0] = 'Francis'
+#ninjas.append('Michael')
+#print(ninjas)
+#ninjas.pop()
+#print(ninjas)
+#ninjas.pop(1)
+#print(ninjas)
+
+
+empty_dict = {}
+new_person = {'name': 'John', 'age': 38, 'weight': 160.2, 'has_glasses': False}
+new_person['name'] = 'Jack'
+new_person['hobbies'] = ['climbing', 'coding']
+print(new_person)
+w = new_person.pop('weight')
+print(w)
+print(new_person)
+
+
