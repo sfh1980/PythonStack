6f5eda70b64a07f0056c2a8792b46273ec3dbb5b
Sean <sfh1980@yahoo.com>
2022-08-29 23:16:22 -0400

    for loop basic 1

---
+++ For_Loop_Basic_I/for_loop_basic1.py
@@ -0,0 +1,53 @@
+#range(start, stop, step)
+#start defaults at zero and exits at number provided
+#stop begins at start number and exits at index of stop number provided
+#step increases by provided integer until reaching index of stop integer
+#for i in range(x, x, x)
+    #print(i)
+
+
+
+#BASIC
+for i in range(150):
+    print(i)
+
+#Multiples of Five
+for i in range(5, 1000, 5):
+    print(i)
+
+#Counting, the Dojo Way -  Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+for i in range(1,100,1):
+    print(i)
+    if i % 5 == 0:
+        print("coding")
+    if i % 10 == 0:
+        print("coding dojo")
+
+#Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+min = 0
+max = 500000
+Oddtotal = 0
+
+for i in range(0, 500000):
+    if(i % 2 !=0):
+        print('{0}'.format(i))
+        Oddtotal = Oddtotal + i
+print("final sum from 0 to {0}={1}".format(i, Oddtotal))
+
+
+#Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+for i in range(2018, 0, -4):
+    print(i)
+
+
+#Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+lowNum = 2
+highNum = 10
+mult = 3
+
+for i in range (lowNum, highNum):
+        if i % mult == 0:
+            print (i)
+
--- test/test.py
+++ test/test.py
@@ -10,13 +10,58 @@
 #print(ninjas)
 
 
-empty_dict = {}
-new_person = {'name': 'John', 'age': 38, 'weight': 160.2, 'has_glasses': False}
-new_person['name'] = 'Jack'
-new_person['hobbies'] = ['climbing', 'coding']
-print(new_person)
-w = new_person.pop('weight')
-print(w)
-print(new_person)
+#empty_dict = {}
+#new_person = {'name': 'John', 'age': 38, 'weight': 160.2, #'has_glasses': False}
+#new_person['name'] = 'Jack'
+#new_person['hobbies'] = ['climbing', 'coding']
+#print(new_person)
+#w = new_person.pop('weight')
+#print(w)
+#print(new_person)
+
+
+#name = "Zen"
+#print("my name is" + name)
+
+#first_name = 'Zen'
+#last_name = 'Coder'
+#age = 27
+#print(f'My name is {first_name} {last_name} and I am {age} years old.#')
+
+#first_name = "Alana"
+#last_name = "Da Silva"
+#age = 36
+#profession = "Software Developer"
+#years_experience = 5
+
+#greeting = "Hello my name is", first_name, last_name
+
+#print('Hello my name is ' + first_name + last_name)
+
+#print(f'I am {age} years old')
+
+#print("I work as a {}.".format(profession))
+
+#exp_string = "I have worked in the field for {} years."
+#print(exp_string.format(years_experience))
+
+#output = age - years_experience
+
+#print("I started in the field when I was", output, "years old.")
+
+#fruits = ['apple', 'banana', 'orange', 'strawberry']
+#vegetables = ['lettuce', 'cucumber', 'carrots']
+#fruits_and_vegetables = fruits + vegetables
+#print(fruits_and_vegetables)
+#salad = 3 * vegetables
+#print(salad)
+
+
+def flex_countdown(low, high, mult):
+    for i in range (low, high):
+        if i % mult == 0:
+            print (i)
+            
+flex_countdown(2, 9, 3)
 
 
